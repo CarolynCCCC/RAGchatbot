d2649b51100b1403ffa669fcc738998156fa1357
CarolynCCCC <126298896+CarolynCCCC@users.noreply.github.com>
2023-12-21 22:27:51 +0800

    memeda

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import chainlit as cl 
-
 import PyPDF2
 from docx import Document
 from io import BytesIO
@@ -11,10 +10,7 @@ from langchain.schema import Document
 from chromadb.config import Settings
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chat_models import ChatOpenAI
-#from gpt4all import GPT4All
-import openai
 from openai import OpenAI
-import spacy
 import os
 import sys
 from typing import Optional
@@ -25,10 +21,11 @@ import pyttsx3
 from typing import List
 from langchain.schema.embeddings import Embeddings
 from langchain.vectorstores.base import VectorStore
-from prompt import EXAMPLE_PROMPT, PROMPT, WELCOME_MESSAGE
+from prompt import EXAMPLE_PROMPT, PROMPT, FILE_QUERY, BASIC_QUERY, WELCOMINGS
 #from langchain.llms import Cohere
 #from langchain.llms import Together
 
+default = "query" #or basic
 # llms
 llm = ChatOpenAI()
 # llm = Together(
@@ -52,25 +49,6 @@ def backoff(attempt : int) -> float:
 
 # local LLM but my computer sucks 
 #gpt = GPT4All("gpt4all-falcon-q4_0.gguf")
-
-def get_completion(question):
-    
-    client = OpenAI()
-    response = client.chat.completions.create(
-        model = "gpt-3.5-turbo",
-        messages = [
-            {
-                "role":"assistant",
-                "content":question
-            },
-            {
-                "role":"system",
-                "content":"You are a helpful assistant. You will do some jokes if the user is sad."
-            }
-        ],
-        temperature = 0.5,
-    )
-    return response.choices[0].message.content
 
 def get_pdf_text(file):
     
@@ -132,14 +110,6 @@ def create_search_engine(
 
     return search_engine
 
-async def get_vectorstore(text_chunks, metadatas):
-    embeddings = OpenAIEmbeddings()
-    #embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
-    vstore = await cl.make_async(create_search_engine)(
-        docs = text_chunks, embeddings = embeddings, metadatas = metadatas
-    )
-    return vstore
-
 def get_conversation_chain(vectorstore):
     #llm = huggingface_hub.HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
     chain = RetrievalQAWithSourcesChain.from_chain_type(
@@ -150,6 +120,25 @@ def get_conversation_chain(vectorstore):
     )
     return chain
 
+def get_completion(question):
+    
+    client = OpenAI()
+    response = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {
+                "role":"user",
+                "content":question
+            },
+            {
+                "role":"system",
+                "content":"You are a helpful assistant."
+            }
+        ],
+        temperature = 0.5,
+    )
+    return response.choices[0].message.content
+
 def speak_text(text):
     engine = pyttsx3.init()
     engine.say(text)
@@ -158,65 +147,138 @@ def speak_text(text):
 #continuously on a loop
 @cl.on_message
 async def main(message: cl.Message):
-    # get previous chain
-    chain = cl.user_session.get("chain")  
-    cb = cl.AsyncLangchainCallbackHandler(
-        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
-    )
-    cb.answer_reached = True
-    res = await chain.acall(message.content, callbacks=[cb])  
-
-    for attempt in range(max_retries):
-        try:
-            res = await chain.acall(message.content, 
-                                    callbacks = [cl.AsyncLangchainCallbackHandler()])
-            break
-        except Exception:
-            wait_time = backoff(attempt)
-            logger.exception(f"Rate limit reached. Waiting {wait_time} seconds and trying again")
-            time.sleep(wait_time)
-            break
+    chat_type = cl.user_session.get("actions")
     
-    answer = res["answer"]
-    
-    sources = res["sources"].strip()
-    source_elements = []
+    if chat_type == "basic":
+        res = get_completion(message.content)
+        await cl.Message(content=res, 
+                    author="Chatbot").send()
+        answer = res
 
-    metadatas = cl.user_session.get("metadatas")
-    all_sources = [m["source"] for m in metadatas]
-    texts = cl.user_session.get("texts")
+    elif chat_type == "file":
+        chain = cl.user_session.get("chain") 
 
-    if sources != "":
-        found_sources = []
-
-        # Add the sources to the message
-        for source in sources.split(","):
-            source_name = source.strip().replace(".", "")
-            # Get the index of the source
+        cb = cl.AsyncLangchainCallbackHandler(
+            stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+        )
+        cb.answer_reached = True
+        for attempt in range(max_retries):
             try:
-                index = all_sources.index(source_name)
-            except ValueError:
-                continue
-            text = texts[index]
-            found_sources.append(source_name)
-            # Create the text element referenced in the message
-            source_elements.append(cl.Text(content=text, name=source_name))
+                res = await chain.acall(message.content, 
+                                        callbacks = [cb])
+                break
+            except Exception:
+                wait_time = backoff(attempt)
+                logger.exception(f"Rate limit reached. Waiting {wait_time} seconds and trying again")
+                time.sleep(wait_time)
+                break
 
-        if found_sources:
-            answer += f"\nSources: {', '.join(found_sources)}"
+        answer = res["answer"]
+        sources = res["sources"].strip()
+        source_elements = []
+
+        metadatas = cl.user_session.get("metadatas")
+        all_sources = [m["source"] for m in metadatas]
+        texts = cl.user_session.get("texts")
+
+        if sources != "":
+            found_sources = []
+
+            # Add the sources to the message
+            for source in sources.split(","):
+                source_name = source.strip().replace(".", "")
+                # Get the index of the source
+                try:
+                    index = all_sources.index(source_name)
+                except ValueError:
+                    continue
+                text = texts[index]
+                found_sources.append(source_name)
+                # Create the text element referenced in the message
+                source_elements.append(cl.Text(content=text, name=source_name))
+
+            if found_sources:
+                answer += f"\nSources: {', '.join(found_sources)}"
+            else:
+                answer += "\nNo source is found. "
+        
+        if cb.has_streamed_final_answer:
+            cb.final_stream.elements = source_elements
+            await cb.final_stream.update()
         else:
-            answer += "\nNo source is found. "
-    
-
-    if cb.has_streamed_final_answer:
-        cb.final_stream.elements = source_elements
-        await cb.final_stream.update()
+            await cl.Message(content=answer, 
+                    elements=source_elements, 
+                    author="Chatbot").send()
     else:
-        await cl.Message(content=answer, 
-                   elements=source_elements, 
-                   author="Chatbot").send()
+        await cl.Message(content="Please select one of the option to start!",
+                         author="Chatbot").send()
     #speak_text(answer)
 
+# see user wants basic or file query
+@cl.action_callback(name="confirm")
+async def action_callback(action: cl.Action):
+    
+    value = action.value
+
+    actions = cl.user_session.get("actions")
+    if actions:
+        for action in actions:
+            await action.remove()
+        cl.user_session.set("actions", None)
+    
+
+    if value == '1':
+        cl.user_session.set("actions", "basic")
+        
+        await cl.Message(content=BASIC_QUERY,
+                         author="Chatbot").send()
+        
+    elif value == '0':
+        
+        cl.user_session.set("actions", "file")
+        files = None  
+        while files == None:
+            files = await cl.AskFileMessage(
+                content = FILE_QUERY, 
+                accept=["application/pdf",
+                        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                        "text/plain"],
+                author = "Chatbot",
+                max_size_mb = 20,
+                timeout = 86400,
+                raise_on_timeout = False
+            ).send()
+
+        file = files[0]
+
+        msg = cl.Message(
+            content=f"Processing '{file.name}' ...",
+            author = "Chatbot",
+        )
+        await msg.send()
+
+        text = get_pdf_text(file)
+        text_chunks = get_text_chunks(text)
+        metadatas = [{"source": f"{i}-pl"} for i in range(len(text_chunks))]
+
+        #vectorstore = await get_vectorstore(text_chunks, metadatas)
+
+        embeddings = OpenAIEmbeddings()
+        #embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
+        vstore = await cl.make_async(create_search_engine)(
+            docs = text_chunks, embeddings = embeddings, metadatas = metadatas
+        )
+        chain = get_conversation_chain(vstore)
+
+        msg.content = f"Processing '{file.name}' done.\nFeel free to ask any question!"
+        await msg.update()
+
+        cl.user_session.set("metadatas", metadatas)
+        cl.user_session.set("texts", text_chunks)
+        cl.user_session.set("chain",chain)
+    
+    
+# when a new chat is start
 @cl.on_chat_start
 async def start():
     cl.user_session.get("user")
@@ -231,47 +293,26 @@ async def start():
         path="user.jpg"
     ).send()
 
-    files = None  
-    while files == None:
-        files = await cl.AskFileMessage(
-            content = WELCOME_MESSAGE, 
-            accept=["application/pdf",
-                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-                    "text/plain"],
-            author = "Chatbot",
-            max_size_mb = 20,
-            timeout = 86400,
-            raise_on_timeout = False
-        ).send()
+    actions = [
+        cl.Action(name="confirm",label="1. Basic Chat 💬", value="1", description="Have a basic chat!"),
+        cl.Action(name="confirm",label="2. Ask your file 📚",value="0",description="Ask about your files!")
+    ]
 
-    file = files[0]
+    cl.user_session.set("actions", actions)
 
-    msg = cl.Message(
-            content=f"Processing '{file.name}' ...",
-            author = "Chatbot",
+    msg = cl.Message(content=WELCOMINGS, 
+            actions=actions,
+            author="Chatbot",
             )
+    
     await msg.send()
-
-    text = get_pdf_text(file)
-    text_chunks = get_text_chunks(text)
-    metadatas = [{"source": f"{i}-pl"} for i in range(len(text_chunks))]
-
-    vectorstore = await get_vectorstore(text_chunks, metadatas)
-    chain = get_conversation_chain(vectorstore)
-
-    cl.user_session.set("metadatas", metadatas)
-    cl.user_session.set("texts", text_chunks)
-
-    msg.content = f"Processing '{file.name}' done.\nFeel free to ask any question!"
-    await msg.update()
-
-    cl.user_session.set("chain",chain)
+    
 
 @cl.password_auth_callback
 def auth_callback(username: str, password: str) -> Optional[cl.AppUser]:
-  if (username, password) == ("admin", "admin"):
-    return cl.AppUser(username="admin", role="ADMIN", provider="credentials")
-  else:
-    return None
+    if (username, password) == ("admin@gmail.com", "admin"):
+        return cl.AppUser(username="admin", role="ADMIN", provider="credentials")
+    else:
+        return None
     
 
--- prompt.py
+++ prompt.py
@@ -1,13 +1,11 @@
 # flake8: noqa
 from langchain.prompts import PromptTemplate
 
-template = """Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES").
+template = """Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES") below the final answer.
 ALWAYS return a "SOURCES" field in your answer, with the format "SOURCES: <source1>, <source2>, <source3>, ...".
-If you cannot find the answer from the extracted parts of a long document, you can answer with your own knowledge, as you are very smart and can answer general questions, and say it is not provided from the content. 
-You can do common conversation as well; for example, when the question says hi, you will greet back. When the question says sorry, you will say no worries. 
-When the question says thanks, you will say you are welcome. When the question says you are good, you will say thank you. You will answer according to user intent. If you think the user is sad, you can tell a joke.
-
-
+If you cannot find the answer from the extracted parts of a long document, just say you don't know, never try to make up an answer. 
+You can do common chit chat for example when the question is about greeting, you will greet back, if the question is saying sorry, you will say no worries,
+if the question about thanks, you will say welcome.
 
 QUESTION: {question}
 =========
@@ -15,7 +13,7 @@ QUESTION: {question}
 =========
 FINAL ANSWER:"""
 
-conv_temp = ""
+
 
 PROMPT = PromptTemplate(
     template=template, input_variables=["summaries", "question"]
@@ -26,9 +24,19 @@ EXAMPLE_PROMPT = PromptTemplate(
     input_variables=["page_content", "source"],
 )
 
-WELCOME_MESSAGE = """\
-👋 Hello there! Welcome to Pandai Chat~
-To get started:
+
+FILE_QUERY = """\
 1. Upload a .pdf, .txt, or .docx file
 2. Ask any question about the file!
 """
+
+BASIC_QUERY = """\
+Hi! Feel free to ask any questions~
+"""
+
+WELCOMINGS = """\
+👋 Hello there! Welcome to Pandai Chat~
+We are delighted to have you here. Pandai Chat is your intelligent companion designed to assist you effortlessly. Whether you seek engaging conversation or require assistance with file-related tasks, our chatbot is here to cater to your needs.
+\nTo get started, kindly choose from the following options: 
+"""
+
